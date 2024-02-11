23287942f4b5bd4c7b8c704a7418d0cf86f5609b
impati <yongs170@naver.com>
2024-02-11 22:18:43 +0900

    feature: 2번째 가테스트 추가

--- product-domain/src/main/kotlin/com/example/productdomain/Product.kt
+++ product-domain/src/main/kotlin/com/example/productdomain/Product.kt
@@ -1,10 +1,15 @@
 package com.example.productdomain
 
 class Product(
-    var name: String
+    var name: String,
+    var age: Int
 ) {
 
     fun changeName(newName: String) {
         this.name = newName;
+    }
+
+    fun changeAge(age: Int) {
+        this.age = age;
     }
 }
