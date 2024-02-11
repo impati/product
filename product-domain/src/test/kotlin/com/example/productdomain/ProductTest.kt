4e264016e5fdc4ba0df1de2a0a8be7b833f34963
impati <yongs170@naver.com>
2024-02-11 22:21:42 +0900

    feature: 2번째 가테스트 테스트 추가

--- product-domain/src/test/kotlin/com/example/productdomain/ProductTest.kt
+++ product-domain/src/test/kotlin/com/example/productdomain/ProductTest.kt
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.Test
 class ProductTest {
 
     @Test
-    fun test() {
+    fun changeName() {
         val product = Product("ice")
 
         product.changeName("ice cream")
 
         assertThat(product.name).isEqualTo("ice cream")
+    }
+
+    @Test
+    fun changeAge() {
+        val product = Product("ice", 25)
+
+        product.changeAge(26)
+
+        assertThat(product.age).isEqualTo(26)
     }
 }
