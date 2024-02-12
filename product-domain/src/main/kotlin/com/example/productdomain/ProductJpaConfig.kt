66153963a2b9e0cf5ce20e070d09fbe1b56f5ee1
impati <yongs170@naver.com>
2024-02-12 20:08:40 +0900

    feature: JPA 설정

---
+++ product-domain/src/main/kotlin/com/example/productdomain/ProductJpaConfig.kt
@@ -0,0 +1,60 @@
+package com.example.productdomain
+
+import com.zaxxer.hikari.HikariDataSource
+import jakarta.persistence.EntityManagerFactory
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import org.springframework.transaction.PlatformTransactionManager
+import javax.sql.DataSource
+
+@Configuration
+@EnableConfigurationProperties
+@EnableJpaRepositories(
+    basePackageClasses = [ProductDomainBase::class]
+)
+@EntityScan(basePackageClasses = [ProductDomainBase::class])
+class ProductJpaConfig {
+
+
+    @Bean("entityManagerFactory")
+    fun entityManagerFactory(
+        jpaProperties: JpaProperties,
+        hibernateProperties: HibernateProperties
+    ): LocalContainerEntityManagerFactoryBean {
+        return EntityManagerFactoryBuilder(HibernateJpaVendorAdapter(), jpaProperties.properties, null)
+            .dataSource(dataSource())
+            .properties(hibernateProperties.determineHibernateProperties(jpaProperties.properties, HibernateSettings()))
+            .persistenceUnit("productDomain")
+            .packages(ProductDomainBase::class.java)
+            .build()
+    }
+
+    @Bean("transactionManager")
+    fun platformTransactionManager(entityManagerFactory: EntityManagerFactory): PlatformTransactionManager {
+        return JpaTransactionManager(entityManagerFactory)
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.jpa")
+    fun jpaProperties(): JpaProperties = JpaProperties()
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.jpa.hibernate")
+    fun hibernateProperties(): HibernateProperties = HibernateProperties()
+
+    @Bean("datasource")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    fun dataSource(): DataSource = DataSourceBuilder.create().type(HikariDataSource::class.java).build();
+}
--- product-domain/src/main/kotlin/com/example/productdomain/product/domain/Product.kt
+++ product-domain/src/main/kotlin/com/example/productdomain/product/domain/Product.kt
@@ -1,11 +1,27 @@
 package com.example.productdomain.product.domain
 
+import jakarta.persistence.*
 
+
+@Entity
+@Table(name = "products")
 class Product(
+
+    @Column(name = "name")
     val name: String,
+
+    @Column(name = "price")
     val price: Int,
+
+    @Column(name = "quantity")
     val quantity: Int,
+
+    @Column(name = "status")
     val status: ProductStatus = ProductStatus.PRE_REGISTRATION,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
 ) {
 
     init {
