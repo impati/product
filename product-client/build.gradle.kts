f05dd6634c5f9a1db9e71c210017b1016e352187
impati <yongs170@naver.com>
2024-03-09 22:45:06 +0900

    feature: product-client 모듈에 bootJar =  false 설정

--- product-client/build.gradle.kts
+++ product-client/build.gradle.kts
@@ -1,5 +1,14 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 }
 
 
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
