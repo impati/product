9df01cc9d452592f2c6f521cbb6061b930b4b095
impati <yongs170@naver.com>
2024-03-29 22:11:57 +0900

    refactor: 상품에서 버전체크하는 것으로 리팩토링
    - "가져와서 처리하지말고 시켜라" 원칙

--- product-domain/src/main/kotlin/com/example/productdomain/product/domain/ProductEditor.kt
+++ product-domain/src/main/kotlin/com/example/productdomain/product/domain/ProductEditor.kt
@@ -1,7 +1,6 @@
 package com.example.productdomain.product.domain
 
 import com.example.productdomain.common.UpdatedAudit
-import com.example.productdomain.product.exception.ProductOptimisticException
 import com.example.productdomain.util.findByIdOrThrow
 import lombok.extern.slf4j.Slf4j
 import org.springframework.stereotype.Component
@@ -26,9 +25,7 @@ class ProductEditor(
     ): Product {
         val product: Product = productRepository.findByIdOrThrow(productId)
 
-        if (product.version != version) {
-            throw ProductOptimisticException("수정에 실패했습니다. 다시 시도해주세요.")
-        }
+        product.checkVersion(version)
 
         return product.apply { update(updatedAudit, name, price, quantity, status) }
     }
