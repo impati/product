e146944a90c12d7bb2a25a6b95dbf879ab22d101
impati <yongs170@naver.com>
2024-04-14 16:15:54 +0900

    feature: productStatusEvent 를 받아서 비동기로 인덱싱을 prod , beta 에서만 하도록 변경

--- product-search/src/main/kotlin/com/example/productsearch/config/AsyncConfig.kt
+++ product-search/src/main/kotlin/com/example/productsearch/config/AsyncConfig.kt
@@ -1,8 +1,10 @@
 package com.example.productsearch.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableAsync
 
+@Profile("prod | beta")
 @EnableAsync
 @Configuration
 class AsyncConfig {
