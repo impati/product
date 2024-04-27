f7b53a7d8f09e26f7653b647eb0f1bf9b9ead040
impati <yongs170@naver.com>
2024-04-27 18:04:02 +0900

    feature: ProductHistory 에 image 정보 추가

---
+++ product-domain/src/main/resources/db/migration/V20240427174551__add_image_product_history.sql
@@ -0,0 +1,2 @@
+ALTER TABLE product_histories
+    ADD COLUMN image varchar(255) NOT NULL;
--- product-domain/src/test/kotlin/com/example/productdomain/product/domain/ProductHistoryTest.kt
+++ product-domain/src/test/kotlin/com/example/productdomain/product/domain/ProductHistoryTest.kt
@@ -23,14 +23,16 @@ class ProductHistoryTest {
                 ProductHistory::name,
                 ProductHistory::price,
                 ProductHistory::status,
-                ProductHistory::productId
+                ProductHistory::productId,
+                ProductHistory::imagePath
             )
             .contains(
                 CreatedAudit(product.updatedAudit.updatedAt, product.updatedAudit.updatedBy),
                 product.name.value,
                 product.price.value,
                 product.status,
-                product.id
+                product.id,
+                product.imagePath.value
             )
     }
 
@@ -55,7 +57,8 @@ class ProductHistoryTest {
                 ProductHistory::name,
                 ProductHistory::price,
                 ProductHistory::status,
-                ProductHistory::productId
+                ProductHistory::productId,
+                ProductHistory::imagePath
             )
             .contains(
                 CreatedAudit(LocalDateTime.of(2024, 3, 16, 0, 0), "root"),
@@ -63,7 +66,8 @@ class ProductHistoryTest {
                 1000000,
                 1000000,
                 ProductStatus.SELLING,
-                product.id
+                product.id,
+                "https://localhost"
             )
     }
 
