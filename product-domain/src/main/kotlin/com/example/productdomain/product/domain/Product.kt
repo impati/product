536082098445fd5b69c21e90ea9cdf8c6ceca287
impati <yongs170@naver.com>
2024-04-27 16:41:12 +0900

    feature: product imagePath 컬럼 추가

--- product-domain/src/main/kotlin/com/example/productdomain/product/domain/Product.kt
+++ product-domain/src/main/kotlin/com/example/productdomain/product/domain/Product.kt
@@ -1,6 +1,7 @@
 package com.example.productdomain.product.domain
 
 import com.example.productdomain.common.CreatedAudit
+import com.example.productdomain.common.ImagePath
 import com.example.productdomain.common.UpdatedAudit
 import com.example.productdomain.product.exception.ProductOptimisticException
 import jakarta.persistence.*
@@ -35,6 +36,10 @@ class Product(
     @AttributeOverride(name = "value", column = Column(name = "quantity"))
     var quantity: ProductQuantity,
 
+    @Embedded
+    @AttributeOverride(name = "value", column = Column(name = "image"))
+    var imagePath: ImagePath,
+
     @Enumerated(value = EnumType.STRING)
     @Column(name = "status")
     var status: ProductStatus = ProductStatus.PRE_REGISTRATION,
@@ -53,12 +58,14 @@ class Product(
         name: String,
         price: Int,
         quantity: Int,
+        imagePath: String,
         status: ProductStatus
     ) {
         require(this.status != ProductStatus.DELETED) { "상품이 이미 삭제되어 변경할 수 없습니다." }
         this.name = ProductName(name)
         this.price = ProductPrice(price)
         this.quantity = ProductQuantity(quantity)
+        this.imagePath = ImagePath(imagePath)
         this.status = status
         this.updatedAudit = updatedAudit
     }
